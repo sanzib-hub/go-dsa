30ccf02aa276fef14e35094e557b6bcd64094124
Sanjib Nirola <sanjibnirola@outlook.com>
2024-07-10 08:26:27 +0000

    added:insertion

---
+++ sort/array/insertion/insertion-sort.go
@@ -0,0 +1,24 @@
+package main
+
+
+import (
+	"fmt"
+)
+
+func main(){
+	a:=[5]int{5,4,3,2,1}
+	b:=[]int{}
+	b=append(b, a[:]...)
+	fmt.Println("Before sort",b)
+
+	for i:=1;i<len(b);i++{
+		index:=b[i]
+		j:=i-1
+		for j>=0 && b[j] >index{
+			b[j+1]=b[j]
+			j=j-1
+		}
+		b[j+1] = index
+	}
+	fmt.Println(b)
+}
